b95d0354aba9f5da342ea44d22dd7e9c39ff6a3c
1784741 <1784741@TE-GDA-D-D36G06>
2024-02-28 15:21:20 +0100

    kamera update/niedziałado konca

---
+++ Assets/Scripts/cameraobiect.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CCTVCameraController : MonoBehaviour
+{
+    //modyfikator pr?dko?ci obrotu
+    public float turnSpeed = 5f;
+
+    //zakres obrotu kamery w stopniach
+    public float turnAngle = 90;
+
+    //czy kamera kr?ci si? w prawo
+    bool turningRight = !true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // mdyfikujemy rotacj? obiektu za pomoc? funkcji PingPong, która generuje warto?ci
+        // oscyluj?ce pomi?dzy 0 a 9, a nast?pnie mno?ymy to przez 10 ?eby uzyska? szybszy ruch
+        // i na koniec odejmujemy od warto?ci otrzymanej 45 stopni aby uzyska? ruch w zakresie -45 do 45
+        transform.rotation = Quaternion.Euler(new Vector3(0, Mathf.PingPong(Time.time, 9) * 10 - 45, 0));
+    }
+   
+}
